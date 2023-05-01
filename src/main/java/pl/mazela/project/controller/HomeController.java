0f6a1f5cc7ba5f1c6e1f4374e1bf3f0a27221444
Anna Mazela <aniamazela@gmail.com>
2023-05-01 15:18:44 +0200

    +if login has errors

--- src/main/java/pl/mazela/project/controller/HomeController.java
+++ src/main/java/pl/mazela/project/controller/HomeController.java
@@ -2,10 +2,14 @@ package pl.mazela.project.controller;
 
 import java.util.Date;
 
+import javax.validation.Valid;
+
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import pl.mazela.project.models.User;
@@ -31,7 +35,7 @@ public class HomeController {
     }  
 
     @GetMapping("/login")
-    public String registerForm(Model model) {
+    public String registerForm(Model model, @Valid User user, Errors errors) {
         model.addAttribute("user", new User());
         return "login";
     }
--- src/main/java/pl/mazela/project/repositories/UserRepository.java
+++ src/main/java/pl/mazela/project/repositories/UserRepository.java
@@ -2,12 +2,13 @@ package pl.mazela.project.repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import pl.mazela.project.models.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends CrudRepository<User, Long>{
     User findByUsername(String username);
 
 
